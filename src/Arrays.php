a284bf5c325a9ad2e01d06b966782d0b28012c78
fuwei <542928955@qq.com>
2019-11-07 10:37:12 +0800

    新增冒泡排序，快速排序，二分查找，汉字拼音首字母

--- src/Arrays.php
+++ src/Arrays.php
@@ -52,4 +52,97 @@ class Arrays
         $arrayFlip = array_flip($array);
         return isset($arrayFlip[$key]);
     }
+
+    /**
+     * 冒泡排序
+     * @param $array
+     * @param string $sort
+     * @return mixed
+     */
+    public static function bubbSort($array, $sort = 'desc')
+    {
+        if(empty($array)) return $array;
+        //默认有序
+        $isSorted   = true;
+        $Count     = count($array);
+        for($i = 0; $i < $Count; $i++) {
+            //对比次数随着循环逐渐减少，因为后面的数据已经处理为有序
+            for($j = 0; $j < ($Count - $i - 1); $j++) {
+                //执行判断
+                $isChange = $sort == 'desc' ? $array[$j] < $array[$j+1] : $array[$j] > $array[$j+1];
+                if($isChange) {
+                    //首次对比，判断是否有序
+                    $isSorted       = false;
+                    $temp           = $array[$j];
+                    $array[$j]      = $array[$j+1];
+                    $array[$j+1]    = $temp;
+                }
+            }
+            if($isSorted) break;
+        }
+        unset($i, $j, $isSorted, $temp, $Count);
+        return $array;
+    }
+
+    /**
+     * 快速排序
+     * @param $array
+     * @param string $sort
+     * @return array
+     */
+    public static function quickSort($array, $sort = 'desc')
+    {
+        //检查数据，多于一个数据才执行
+        $nCount = count($array);
+        if($nCount > 1) {
+            //选取标准（第一个数据）
+            $nStandard = $array[0];
+            $arrLeftData = [];
+            $arrRightData = [];
+            //遍历，注意这里从1开始比较
+            for($i = 1; $i < $nCount; $i++) {
+                if($sort == 'desc') {
+                    $array[$i] > $nStandard ? $arrLeftData[] = $array[$i] : $arrRightData[] = $array[$i];
+                } else {
+                    $array[$i] > $nStandard ? $arrRightData[] = $array[$i] : $arrLeftData[] = $array[$i];
+                }
+            }
+            $array = array_merge(
+                self::quickSort($arrLeftData, $sort),
+                array($nStandard),
+                self::quickSort($arrRightData, $sort)
+            );
+        }
+        return $array;
+    }
+
+    /**
+     * 二分查找
+     * @param $toSearch
+     * @param $array
+     * @return bool|int
+     */
+    public static function twoPointSearch($toSearch, $array)
+    {
+        //确定当前的检索范围
+        $nCount = count($array);
+        //低位键，初始为0
+        $nLowNum = 0;
+        //高位键，初始为末尾
+        $nHighNum = $nCount - 1;
+        while($nLowNum <= $nHighNum) {
+            //选定大概中间键
+            $nMiddleNum = intval(($nHighNum + $nLowNum)/2);
+            if($array[$nMiddleNum] > $toSearch) {
+                //比检索值大
+                $nHighNum = $nMiddleNum - 1;
+            } elseif ($array[$nMiddleNum] < $toSearch) {
+                //比检索值小
+                $nLowNum = $nMiddleNum + 1;
+            } else {
+                return $nMiddleNum;
+            }
+        }
+        return false;
+    }
 }
